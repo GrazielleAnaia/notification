7dda5c3ea7bf75709eaba856b895dacacf9ee818
Grazielle <grazielle.anaia@gmail.com>
2025-01-03 09:25:42 -0500

    commit initial

---
+++ src/test/java/com/grazielleanaia/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
